cb9d77387d583d6ab24e8367cf18d7730d81f1af
p-argolo <paula.reis@sergipetec.org.br>
2025-06-26 14:49:38 -0300

    :sparkles: feat: rota do portal via vercel

--- src/main/java/com/BaneseLabes/portal/config/CorsConfig.java
+++ src/main/java/com/BaneseLabes/portal/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // libera todos os endpoints
-                        .allowedOrigins("http://localhost:3000") // frontend
+                        .allowedOrigins("https://portal-front-bice.vercel.app") // frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
